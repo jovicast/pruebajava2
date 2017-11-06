0ebcf106e25445f71d4ccebe820315d5071906e9
jovicast <myriamcastillo630@gmail.com>
2017-11-06 19:52:40 -0300

    iniciando clase 2 de contraseña

---
+++ src/pruebajava2/Contrseña.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+
+import javax.swing.JOptionPane;
+
+/************************************
+ * Myriam Castillo Zavala;
+ * @author LAB07
+ * Fecha: Noviembre 06  2017
+ * Programa: Pruebajava2 
+ * Responsabilidad: Aplicaciones Escritorio Java
+ ************************************/
+public class Contrseña 
+
+{
+ public static void main(String[] args) 
+ {
+     int contraseña;
+ 
+  contraseña = Integer.parseInt(JOptionPane.showInputDialog("ingrese clave"));
+     int clave1;
+     int clave2;
+     int clave3;
+     int bloqueada;
+     
+     if(clave1, clave2,clave3);//Aqui va una condicion
+    {
+        //Si la condicion se cumple se ejecuta este codigo
+     JOptionPane.showMessageDialog(null, "ingresar clave1");   
+    }
+     //Si la condicion se cumple se ejecuta este codigo
+     JOptionPane.showMessageDialog(null, "ingresa clave2");  
+     
+    {
+     JOptionPane.showMessageDialog(null, "ingresa clave3");  
+    }
+    {
+     JOptionPane.showMessageDialog(null, "ingresa clave3");  
+    }
+ }
+    
+    
+}
